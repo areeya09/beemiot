505767b6bd609bf5247af89915378be7372929dd
areeya09 <71056640+areeya09@users.noreply.github.com>
2021-08-13 14:49:52 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,2 @@
-<h6> Areeya Chanakan
+<h2> Areeya Chanakan
+<h3> <iframe src= "https://thingspeak.com/channels/1458434/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15">
