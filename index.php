78cdad4e920f946d522bb972b0ff0162396ae4c8
areeya09 <71056640+areeya09@users.noreply.github.com>
2021-08-13 15:20:39 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<h2> Areeya Chanakan 62111760 </h2><br>
+<h2> <center> Areeya Chanakan 62111760 </h2><br> <center>
 <iframe src= "https://thingspeak.com/channels/1458434/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" width="400" height="200"> </iframe>
 <iframe src= "https://thingspeak.com/channels/1458434/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" width="400" height="200"> </iframe>
 <iframe src= "https://thingspeak.com/channels/1458434/maps/channel_show" width="400" height="200"> </iframe>
