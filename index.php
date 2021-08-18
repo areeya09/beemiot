e870b036487bf8e66aa5bd3a7424074e414521fb
areeya09 <71056640+areeya09@users.noreply.github.com>
2021-08-18 17:11:27 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,3 +2,5 @@
 <iframe src= "https://thingspeak.com/channels/1458434/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" width="450" height="260"> </iframe>
 <iframe src= "https://thingspeak.com/channels/1458434/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" width="450" height="260"> </iframe>
 <iframe src= "https://thingspeak.com/channels/1458434/maps/channel_show" width="450" height="260"> </iframe>
+
+  
